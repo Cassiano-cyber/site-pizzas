0e12b5d6a9afe283faa3c3b8052514d760296498
Cassiano-cyber <00001095269859SP@al.educacao.sp.gov.br>
2024-11-10 15:35:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
-document.getElementById('reservationForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-
-    const name = document.getElementById('name').value;
-    const date = document.getElementById('date').value;
-    const time = document.getElementById('time').value;
-
-    const message = `Reserva realizada com sucesso para ${name} na data ${date} às ${time}.`;
-    document.getElementById('reservationMessage').textContent = message;
-
-    // Limpa o formulário
-    document.getElementById('reservationForm').reset();
+// Função para simular envio de formulário
+document.getElementById("contact-form").addEventListener("submit", function(event) {
+    event.preventDefault(); // Impede o envio real do formulário
+    alert("Sua mensagem foi enviada com sucesso!");
 });
